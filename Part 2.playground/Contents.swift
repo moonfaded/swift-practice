88a7c1c3ed4b1eacb569ad510eb3547d1146259a
Alexander Vladyko <vladyko.alexander@gmail.com>
2022-01-02 23:39:22 +0300

    some practice with tuple data type

--- Part 2.playground/Contents.swift
+++ Part 2.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//  Some practice with tuple data type
+
+var myFirstTuple = ("my", 1, "tuple")
+
+var mySecondTuple: (String, Int, String)
+mySecondTuple = ("my", 2, "tuple")
+
+print("First tuple = \(myFirstTuple), second tuple = \(mySecondTuple)")
+
+print("Second item in first tuple = \(myFirstTuple.1)")
+
+let myThirdTuple: (whosThatTuple: String, numberOfTuple: Int, whatIsThis: String) = ("my", 3, "tuple")
+
+let (ownerOfTuple,_,_) = myThirdTuple
+print(ownerOfTuple)
