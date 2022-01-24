c5f04dd884a057e3ef4dd0b3cc7649593d14bc1e
Alexander Vladyko <vladyko.alexander@gmail.com>
2022-01-24 05:42:59 +0300

    L22 HW1 done

--- Part 4 Non-trivial features of Swift.playground/Contents.swift
+++ Part 4 Non-trivial features of Swift.playground/Contents.swift
@@ -272,3 +272,50 @@ print(userIharBlinau.properties())
 userIharBlinau.changeName(name: "Igor")
 userIharBlinau.changeSurname(surname: "Blinov")
 print(userIharBlinau.properties())
+
+
+
+
+//                  Lesson 22. Properties
+
+//          Homework
+
+/*
+ Задание 1
+
+ 1) Создайте класс RandomNumberGenerator, которы будет возвращать случайно сгенерированное число. Его инициализатор должен принимать свойства min и max, ограничивающие диапазон возможных значений сверху и снизу (максимальное и минимальное значения). Так же у класса должен быть метод getNumber(), возвращающий случайное целое число (из диапазона min...max).
+ 2) Создайте структуру Employee, содержащее три свойства:
+
+ firstName – имя работника
+ secondName – фамилия работника
+ salary – заработная плата
+ Типы данных свойств определите самостоятельно
+ 3) Создайте два массива типа [String] каждый. Первый должен содержать 5 вариантов имен, а второй – пять вариантов фамилий.
+ 4) Создайте экземпляр типа Employee. Значения имени и фамилии должны браться случайным образом из созданных в пункте 3 массивов. Значение заработной платы должно быть случайным целым числом в диапазоне от 20000 до 100000. Используйте созданный в шаге 1) класс RandomNumberGenerator.
+ */
+
+class RandomNumberGenerator {
+    var min: Int
+    var max: Int
+    
+    init(min: Int, max: Int) {
+        (self.min, self.max) = (min, max)
+    }
+    
+    func getNumber() -> Int {
+        return Int.random(in: min...max)
+    }
+}
+
+struct Employee {
+    var firstName: String
+    var secondName: String
+    var salary: Int
+}
+
+var names = ["Alex", "Ihar", "Vasya", "Borya", "Ihnat"]
+var surnames = ["Alekseev", "Ihnatiev", "Vorobiev", "Volodin", "Vasin"]
+
+var guy = Employee(firstName: names.randomElement()!, secondName: surnames.randomElement()!, salary: RandomNumberGenerator(min: 20_000, max: 100_000).getNumber())
+
+print(guy)
